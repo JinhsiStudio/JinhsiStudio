b29ed06d8d35d139b77680aeaf69d011fe3edcd7
Chiichen <chiichen@qq.com>
2024-08-25 23:49:58 +0800

    feat: introduce updater & generate signed program and updater json in workflow

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,7 @@ mod cmd;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
