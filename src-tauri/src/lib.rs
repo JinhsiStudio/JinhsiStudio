5ebe3494465e8d4a28f274a7b4a37ce42dfac75b
Chiichen <chiichen@qq.com>
2024-08-26 18:55:18 +0800

    feat: app updater (#45)
    * feat: introduce updater & generate signed program and updater json in workflow
    
    * fix: build failure on android
    
    * ci : add issue-translator to translate issue into English automatically

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,7 +2,7 @@ mod cmd;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let builder = tauri::Builder::default()
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
@@ -12,7 +12,10 @@ pub fn run() {
             cmd::gacha::get_gachalog_from_local,
             cmd::gacha::update_gachalog_from_url,
             cmd::gacha::update_gachalog_from_local,
-        ])
+        ]);
+    #[cfg(not(any(target_os = "android", target_os = "ios")))]
+    let builder = builder.plugin(tauri_plugin_updater::Builder::new().build());
+    builder
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
