5c2c4eb647ccab8fb82ad1889f06202298366806
Chiichen <chiichen@qq.com>
2024-08-28 00:08:43 +0800

    fix: panic at runtime in dev mode

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,7 +13,7 @@ pub fn run() {
             cmd::gacha::update_gachalog_from_url,
             cmd::gacha::update_gachalog_from_local,
         ]);
-    #[cfg(not(any(target_os = "android", target_os = "ios")))]
+    #[cfg(all(desktop, debug_assertions, dev))]
     let builder = builder.plugin(tauri_plugin_updater::Builder::new().build());
     builder
         .run(tauri::generate_context!())
