b4d4e49f1de3bba701266069d4cd856b226f29ea
Chiichen <chiichen@qq.com>
2024-09-04 23:53:03 +0800

    chore: replace css file with inline tailwind

--- src/components/gacha/gacha-avatar-card.tsx
+++ src/components/gacha/gacha-avatar-card.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./gacha-avatar-card.css";
 import { QuestionOutlined } from "@ant-design/icons";
 import { matchResource } from "./gacha-resource";
 
@@ -13,24 +12,27 @@ export default function GachaAvatarCard(props: gachaAvatarCardProps) {
   const { name, resourceId, number } = props;
   const imagePath = matchResource(name, resourceId);
   const [imageError, setImageError] = useState(false);
+
   const handleError = () => {
     console.error("Cannot find resource");
     setImageError(true);
   };
+
   console.info(imagePath, name, resourceId);
+
   return (
-    <div className="gacha-avatar-card">
+    <div className="w-16 h-24 flex flex-col items-center justify-center rounded-md overflow-hidden border border-gray-200 mt-1 mb-1 mx-1">
       {imageError || !imagePath ? (
-        <QuestionOutlined className="placeholder-icon" />
+        <QuestionOutlined className="text-gray-400 text-base p-px" />
       ) : (
         <img
           src={imagePath}
           alt="small"
-          className="gacha-avatar"
+          className="w-16 h-20 object-cover"
           onError={handleError}
         />
       )}
-      <div className="number">{number}</div>
+      <div className="text-base text-gray-800">{number}</div>
     </div>
   );
 }
