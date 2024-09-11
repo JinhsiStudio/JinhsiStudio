d1fc3227095ba60f0c65d96ffaac5d9708621c6b
Chiichen <chiichen@qq.com>
2024-09-11 20:03:51 +0800

    fix: failed to deploy doc website

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export const shared = defineConfig({
+  base: "/JinhsiStudio",
   title: "JinhsiStudio",
   lastUpdated: true,
   cleanUrls: true,
