41d719b96a84c85df79b3a661656439aaeac97a7
Chiichen <chiichen@qq.com>
2024-10-22 14:37:19 +0800

    feat: refactor router in sidebar with shadcn-ui

--- app/src/components/ui/app-sidebar.tsx
+++ app/src/components/ui/app-sidebar.tsx
@@ -11,125 +11,103 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
 interface AppSidebarProps {
   key: string;
   label: string;
-  url: string;
   icon: React.ExoticComponent;
+  onClick?: (key: string) => void;
 }
 
 export function AppSidebar() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   // const [selectedKeys, setSelectedKeys] = useState(["Home"]);
-  // const { t, i18n } = useTranslation();
   // const { storedValue: appSetting } = useAppSetting();
-  // const handleMenuClick = (key: string, path: string) => {
-  //   setSelectedKeys([...selectedKeys, key]);
-  //   navigate(path);
-  // };
+  const handleMenuClick = (_key: string, path: string) => {
+    navigate(path);
+  };
   const { t } = useTranslation();
   const homeItems: AppSidebarProps[] = [
     {
       key: "Home",
       label: t("Label-Home"),
-      url: "/",
       icon: Home,
+      onClick: (key: string) => {
+        handleMenuClick(key, "/");
+      },
     },
   ];
   const toolItems: AppSidebarProps[] = [
     {
       key: "Gacha",
       label: t("Label-GachaTracker"),
-      url: "/gacha",
       icon: ChartPie,
+      onClick: (key: string) => {
+        handleMenuClick(key, "/gacha");
+      },
     },
   ];
   const dataItems: AppSidebarProps[] = [
     {
       key: "CharacterData",
       label: t("Label-Data-Character"),
-      url: "/data/character",
       icon: Sword,
+      onClick: (key: string) => {
+        handleMenuClick(key, "/data/character");
+      },
     },
   ];
   const profileItems: AppSidebarProps[] = [
     {
       key: "Setting",
       label: t("Label-Settings"),
-      url: "/settings",
       icon: Settings,
+      onClick: (key: string) => {
+        handleMenuClick(key, "/settings");
+      },
     },
   ];
+
+  function renderMenuItems(items: AppSidebarProps[]) {
+    return (
+      <SidebarMenu>
+        {items.map((item) => (
+          <SidebarMenuItem key={item.key}>
+            <SidebarMenuButton onClick={() => item.onClick?.(item.key)}>
+              <item.icon />
+              <span>{item.label}</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        ))}
+      </SidebarMenu>
+    );
+  }
   return (
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>{t("Label-Home")}</SidebarGroupLabel>
           <SidebarGroupContent>
-            <SidebarMenu>
-              {homeItems.map((item) => (
-                <SidebarMenuItem key={item.key}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.label}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
+            {renderMenuItems(homeItems)}
           </SidebarGroupContent>
         </SidebarGroup>
         <SidebarGroup>
           <SidebarGroupLabel>{t("Label-Tools")}</SidebarGroupLabel>
           <SidebarGroupContent>
-            <SidebarMenu>
-              {toolItems.map((item) => (
-                <SidebarMenuItem key={item.key}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.label}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
+            {renderMenuItems(toolItems)}
           </SidebarGroupContent>
         </SidebarGroup>
         <SidebarGroup>
           <SidebarGroupLabel>{t("Label-Data")}</SidebarGroupLabel>
           <SidebarGroupContent>
-            <SidebarMenu>
-              {dataItems.map((item) => (
-                <SidebarMenuItem key={item.key}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.label}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
+            {renderMenuItems(dataItems)}
           </SidebarGroupContent>
         </SidebarGroup>
         <SidebarGroup>
           <SidebarGroupLabel>{t("Label-Profile")}</SidebarGroupLabel>
           <SidebarGroupContent>
-            <SidebarMenu>
-              {profileItems.map((item) => (
-                <SidebarMenuItem key={item.key}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.label}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
+            {renderMenuItems(profileItems)}
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
