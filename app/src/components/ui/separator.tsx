b080a791a020456ded17bc2310da610ed4fb58b6
Chiichen <chiichen@qq.com>
2024-10-25 23:28:44 +0800

    feat: update home page

--- app/src/components/ui/separator.tsx
+++ app/src/components/ui/separator.tsx
@@ -20,7 +20,7 @@ const Separator = React.forwardRef<
       orientation={orientation}
       className={cn(
         "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        orientation === "horizontal" ? "h-[1px] w-10/12" : "h-10/12 w-[1px]",
         className,
       )}
       {...props}
--- app/src/pages/home/index.tsx
+++ app/src/pages/home/index.tsx
@@ -1,5 +1,47 @@
-import TodoPage from "../default/todo";
+import { Separator } from "@/components/ui/separator";
 
 export default function HomePage() {
-  return <TodoPage></TodoPage>;
+  return (
+    <div className="flex flex-col items-center">
+      <div>
+        <img src="./public/jinhsi.webp" alt="LOGO" className="m-8" />
+      </div>
+      <div>
+        <h1
+          className="text-4xl font-semibold"
+          style={{
+            fontFamily:
+              '-apple-system, BlinkMacSystemFont, "Segoe UI", "Noto Sans", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+          }}
+        >
+          JinhsiStudio
+        </h1>
+      </div>
+      <Separator className="mt-6 mb-10" />
+      <div className="flex flex-row items-center mt-2">
+        <img
+          alt="license"
+          src="https://img.shields.io/github/license/JinhsiStudio/JinhsiStudio"
+          className="ml-1"
+        />
+        <img
+          alt="commit"
+          src="https://img.shields.io/github/commit-activity/m/JinhsiStudio/JinhsiStudio"
+          className="ml-1"
+        />
+      </div>
+      <div className="flex flex-row items-center mt-2">
+        <img
+          alt="stars"
+          src="https://img.shields.io/github/stars/JinhsiStudio/JinhsiStudio?style=social"
+          className="ml-1"
+        />
+        <img
+          alt="GitHub all releases"
+          src="https://img.shields.io/github/downloads/JinhsiStudio/JinhsiStudio/total?style=social"
+          className="ml-1"
+        />
+      </div>
+    </div>
+  );
 }
