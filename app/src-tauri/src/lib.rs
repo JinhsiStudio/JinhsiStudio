3d1aff46af8872e3d793bcb242e1703d9d2d6883
Chiichen <chiichen@qq.com>
2024-12-15 13:23:36 +0800

    feat: introduce tauri-plugin-process

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![
             cmd::gacha::get_gachalog_from_url,
             cmd::gacha::get_gachalog_from_local,
